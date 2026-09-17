966b5b0e018c94ef08afaf7d07b312c264c8667e
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2026-09-17 00:35:16 -0600

    update

--- assets/custom.js
+++ assets/custom.js
@@ -7,10 +7,11 @@ x = document.getElementById("bigRedBanner");
 x.innerText =  "THE CITY OF MEDICINE HAT/MHT STILL HAS NOT RENEWED THE CERTIFICATE ON THEIR SITE FOR " + elapsedHours + " HOURS!\n"
 x.innerHTML += "This is a mirrored copy of the <a href='https://myride.mhtransit.ca/'>original website</a> with HTTPS working. It is not the official site.";
 x.innerHTML += "<br />When the certificate is renewed on the main site, this mirror should automatically redirect within 15 minutes.";
-x.innerHTML += "<br />Learn why HSTS and certificate renewal is important and being adopted: <a href='https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security'>link</a>.";
+x.innerHTML += "<br />Learn why HSTS and certificate renewal is important and being adopted: <a href='https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security'>LINK</a>.";
 x.innerHTML += "<br /><img src='assets/bannergraphic.png' style='width: 50px;'></img>";
 x.innerHTML += "<hr>";
 x.innerHTML += "Mirror by g0 - <a href='https://tech.beyondgone.xyz'>homepage</a>";
+x.innerHTML += "Source code for this site is public for review: <a href='https://github.com/TEMPJitterBugger/WebsiteCode'>LINK</a>."
 
 document.getElementById("search_spec_stop_header").remove();
 document.getElementById("search_spec_stop_con").remove();
