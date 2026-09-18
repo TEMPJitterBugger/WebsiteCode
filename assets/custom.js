58f88170f631e24e2746e6196aa772332a2f7f13
TEMPJitterBugger <jitterbug2222@proton.me>
2026-09-17 21:21:51 -0600

    Update banner message for mirrored website

--- assets/custom.js
+++ assets/custom.js
@@ -4,8 +4,8 @@ const timeDifference = new Date() - new Date('2026-09-08T23:59:59Z');
 const elapsedHours   = Math.floor(timeDifference / (1000 * 60 * 60));
 
 x = document.getElementById("bigRedBanner");
-x.innerHTML += "This is a mirrored copy of the <a href='https://myride.mhtransit.ca/'>original website</a> with HTTPS working. The certificate on the original website has not been renewed for " + elapsedHours + " hours. It is not the official site.";
-x.innerHTML += "<br />When the certificate is renewed on the main site, this mirror should automatically redirect within 15 minutes.";
+x.innerHTML += "This is a mirrored copy of the <a href='https://myride.mhtransit.ca/'>original website</a> with HTTPS working. It is not the official site.";
+x.innerHTML += "<br />The certificate on the original website has not been renewed for " + elapsedHours + " hours. When the certificate is renewed on the main site, this mirror should automatically redirect within 15 minutes.";
 x.innerHTML += "<br />Learn why HSTS and certificate renewal is important and being adopted: <a href='https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security'>LINK</a>.";
 x.innerHTML += "<br /><img src='assets/bannergraphic.png' style='width: 50px;'></img>";
 x.innerHTML += "<hr>";
